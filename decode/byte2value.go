78bf8ac7981b0d76b71a270b5fd0c2a8ec040d7d
cosog <740206041@qq.com>
2023-03-14 16:10:15 +0800

    add encode and decode

---
+++ decode/byte2value.go
@@ -0,0 +1,498 @@
+// byte2value.go
+package util_decode
+
+import (
+	"encoding/binary"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func Byte2Value(storeMode string, quantity int, StoreDataType string, IFDataType string, ratio float64, b ...byte) interface{} {
+
+	switch strings.ToLower(StoreDataType) {
+	case "asc": //不是切片格式？？
+		switch strings.ToLower(storeMode) {
+		case "little":
+			fallthrough
+		case "big":
+			fallthrough
+		default:
+			s := string(b)
+			value := s
+			return value
+		}
+	case "byte":
+		if len(b) > 0 {
+
+			switch strings.ToLower(IFDataType) {
+			case "bool":
+				value := make([]bool, 0)
+				for i := 0; i < len(b); i++ {
+					for j := 0; j < 8 && quantity > i*8+j; j++ {
+						if (b[i]>>j)&0b00000001 == 0b00000001 {
+							value = append(value, true)
+						} else {
+							value = append(value, false)
+						}
+					}
+				}
+				return value
+			case "byte":
+				value := make([]byte, 0)
+				for i := 0; i < len(b); i++ {
+					d := float32(int(uint(b[i])))
+					d *= float32(ratio)
+					value = append(value, byte(d))
+				}
+				return value
+			case "int":
+				value := make([]int, 0)
+				for i := 0; i < len(b); i++ {
+					d := float32(int(uint(b[i])))
+					d *= float32(int(ratio))
+					value = append(value, int(d))
+				}
+				return value
+			case "float32":
+				value := make([]float32, 0)
+				for i := 0; i < len(b); i++ {
+					d := float32(int(uint(b[i])))
+					d *= float32(ratio)
+
+					a := strconv.FormatFloat(float64(d), 'f', 2, 32)
+					f, _ := strconv.ParseFloat(a, 32)
+
+					value = append(value, float32(f))
+
+				}
+				// util.FormatOutput(value)
+				return value
+			case "float64":
+				value := make([]float64, 0)
+				for i := 0; i < len(b); i++ {
+					d := float64(int(uint(b[i])))
+					d *= float64(ratio)
+
+					a := strconv.FormatFloat(d, 'f', 2, 64)
+					f, _ := strconv.ParseFloat(a, 64)
+
+					value = append(value, f)
+				}
+				// util.FormatOutput(value)
+				return value
+			}
+		}
+	case "uint16":
+		switch strings.ToLower(storeMode) {
+		case "little":
+			if len(b) > 1 {
+
+				switch strings.ToLower(IFDataType) {
+				case "bool":
+					value := make([]bool, 0)
+
+					for i := 0; i < len(b); i = i + 2 {
+						for j := 0; j < 8 && quantity > i*8+j; j++ {
+							if (b[i]>>j)&0b00000001 == 0b00000001 {
+								value = append(value, true)
+							} else {
+								value = append(value, false)
+							}
+						}
+						for j := 0; j < 8 && quantity > (i+1)*8+j; j++ {
+							if (b[i+1]>>j)&0b00000001 == 0b00000001 {
+								value = append(value, true)
+							} else {
+								value = append(value, false)
+							}
+						}
+					}
+
+					return value
+				case "int":
+					value := make([]int, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(binary.LittleEndian.Uint16(b[i : i+2]))
+						f := float32(d) * float32(ratio)
+						value = append(value, int(f))
+					}
+					return value
+				case "float32":
+					value := make([]float32, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(binary.LittleEndian.Uint16(b[i : i+2]))
+						f32 := float32(d) * float32(ratio)
+
+						a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+						f, _ := strconv.ParseFloat(a, 32)
+
+						value = append(value, float32(f))
+					}
+					// util.FormatOutput(value)
+					return value
+				case "float64":
+					value := make([]float64, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(binary.LittleEndian.Uint16(b[i : i+2]))
+						f64 := float64(d) * float64(ratio)
+
+						a := strconv.FormatFloat(f64, 'f', 2, 64)
+						f, _ := strconv.ParseFloat(a, 64)
+
+						value = append(value, f)
+					}
+					// util.FormatOutput(value)
+					return value
+				}
+
+			}
+
+		case "big":
+			fallthrough
+		default:
+			if len(b) > 1 {
+
+				switch strings.ToLower(IFDataType) {
+				case "bool":
+					value := make([]bool, 0)
+
+					for i := 0; i < len(b); i = i + 2 {
+						for j := 0; j < 8 && quantity > i*8+j; j++ {
+							if (b[i+1]>>j)&0b00000001 == 0b00000001 {
+								value = append(value, true)
+							} else {
+								value = append(value, false)
+							}
+						}
+						for j := 0; j < 8 && quantity > (i+1)*8+j; j++ {
+							if (b[i]>>j)&0b00000001 == 0b00000001 {
+								value = append(value, true)
+							} else {
+								value = append(value, false)
+							}
+						}
+					}
+
+					return value
+				case "int":
+					value := make([]int, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(binary.BigEndian.Uint16(b[i : i+2]))
+						f := float32(d) * float32(ratio)
+						value = append(value, int(f))
+					}
+					return value
+				case "float32":
+					value := make([]float32, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(binary.BigEndian.Uint16(b[i : i+2]))
+						f32 := float32(d) * float32(ratio)
+
+						a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+						f, _ := strconv.ParseFloat(a, 32)
+						value = append(value, float32(f))
+					}
+					// util.FormatOutput(value)
+					return value
+				case "float64":
+					value := make([]float64, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(binary.BigEndian.Uint16(b[i : i+2]))
+						f64 := float64(d) * float64(ratio)
+
+						a := strconv.FormatFloat(f64, 'f', 2, 64)
+						f, _ := strconv.ParseFloat(a, 64)
+
+						value = append(value, f)
+					}
+					// util.FormatOutput(value)
+					return value
+				}
+			}
+		}
+	case "int16":
+		switch strings.ToLower(storeMode) {
+		case "little":
+			if len(b) > 1 {
+
+				switch strings.ToLower(IFDataType) {
+				case "int":
+					value := make([]int, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(int16(binary.LittleEndian.Uint16(b[i : i+2])))
+						f := float32(d) * float32(ratio)
+						value = append(value, int(f))
+					}
+					return value
+				case "float32":
+					value := make([]float32, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(int16(binary.LittleEndian.Uint16(b[i : i+2])))
+						f32 := float32(d) * float32(ratio)
+
+						a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+						f, _ := strconv.ParseFloat(a, 32)
+
+						value = append(value, float32(f))
+					}
+					// util.FormatOutput(value)
+					return value
+				case "float64":
+					value := make([]float64, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(int16(binary.LittleEndian.Uint16(b[i : i+2])))
+						f64 := float64(d) * float64(ratio)
+
+						a := strconv.FormatFloat(f64, 'f', 2, 64)
+						f, _ := strconv.ParseFloat(a, 64)
+
+						value = append(value, f)
+					}
+					// util.FormatOutput(value)
+					return value
+				}
+			}
+		case "big":
+			fallthrough
+		default:
+			if len(b) > 1 {
+
+				switch strings.ToLower(IFDataType) {
+				case "int":
+					value := make([]int, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(int16(binary.BigEndian.Uint16(b[i : i+2])))
+						f := float32(d) * float32(ratio)
+						value = append(value, int(f))
+					}
+					return value
+				case "float32":
+					value := make([]float32, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(int16(binary.BigEndian.Uint16(b[i : i+2])))
+						f32 := float32(d) * float32(ratio)
+
+						a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+						f, _ := strconv.ParseFloat(a, 32)
+
+						value = append(value, float32(f))
+					}
+					// util.FormatOutput(value)
+					return value
+				case "float64":
+					value := make([]float64, 0)
+					for i := 0; i < len(b); i = i + 2 {
+						d := int(int16(binary.BigEndian.Uint16(b[i : i+2])))
+						f64 := float64(d) * float64(ratio)
+
+						a := strconv.FormatFloat(f64, 'f', 2, 64)
+						f, _ := strconv.ParseFloat(a, 64)
+
+						value = append(value, f)
+					}
+					// util.FormatOutput(value)
+					return value
+				}
+
+			}
+		}
+	case "int64":
+		switch strings.ToLower(storeMode) {
+		case "little":
+			if len(b) > 1 {
+
+				switch strings.ToLower(IFDataType) {
+
+				case "int64":
+					value := make([]int64, 0)
+					for i := 0; i < len(b); i = i + 8 {
+						d := int64(binary.LittleEndian.Uint64(b[i : i+8]))
+						f := float64(d) * float64(ratio)
+						value = append(value, int64(f))
+					}
+					return value
+
+				}
+			}
+		case "big":
+			fallthrough
+		default:
+			if len(b) > 1 {
+
+				switch strings.ToLower(IFDataType) {
+
+				case "int64":
+					value := make([]int64, 0)
+					for i := 0; i < len(b); i = i + 8 {
+						d := int64(binary.BigEndian.Uint64(b[i : i+8]))
+						f := float64(d) * float64(ratio)
+						value = append(value, int64(f))
+					}
+					return value
+
+				}
+
+			}
+		}
+	case "float32":
+		switch strings.ToLower(storeMode) {
+		case "little":
+			if len(b) > 3 {
+
+				switch strings.ToLower(IFDataType) {
+				case "int":
+					value := make([]int, 0)
+					for i := 0; i < len(b); i = i + 4 {
+						f := math.Float32frombits(binary.LittleEndian.Uint32(b[i : i+4]))
+						d := f * float32(ratio)
+						value = append(value, int(d))
+					}
+					return value
+				case "float32":
+					value := make([]float32, 0)
+					for i := 0; i < len(b); i = i + 4 {
+						f := math.Float32frombits(binary.LittleEndian.Uint32(b[i : i+4]))
+						f32 := float32(f) * float32(ratio)
+
+						a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+						ff, _ := strconv.ParseFloat(a, 32)
+
+						value = append(value, float32(ff))
+					}
+					// util.FormatOutput(value)
+					return value
+				case "float64":
+					value := make([]float64, 0)
+					for i := 0; i < len(b); i = i + 4 {
+						f := math.Float32frombits(binary.LittleEndian.Uint32(b[i : i+4]))
+						f64 := float64(f) * float64(ratio)
+
+						a := strconv.FormatFloat(f64, 'f', 2, 64)
+						ff, _ := strconv.ParseFloat(a, 64)
+
+						value = append(value, ff)
+					}
+					// util.FormatOutput(value)
+					return value
+				}
+
+			}
+		case "big":
+			fallthrough
+		default:
+			if len(b) > 3 {
+
+				switch strings.ToLower(IFDataType) {
+				case "int":
+					value := make([]int, 0)
+					for i := 0; i < len(b); i = i + 4 {
+						f := math.Float32frombits(binary.BigEndian.Uint32(b[i : i+4]))
+						d := f * float32(ratio)
+						value = append(value, int(d))
+					}
+					return value
+				case "float32":
+					value := make([]float32, 0)
+					for i := 0; i < len(b); i = i + 4 {
+						f := math.Float32frombits(binary.BigEndian.Uint32(b[i : i+4]))
+						f32 := float32(f) * float32(ratio)
+
+						a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+						ff, _ := strconv.ParseFloat(a, 32)
+
+						value = append(value, float32(ff))
+					}
+					// util.FormatOutput(value)
+					return value
+				case "float64":
+					value := make([]float64, 0)
+					for i := 0; i < len(b); i = i + 4 {
+						f := math.Float32frombits(binary.BigEndian.Uint32(b[i : i+4]))
+						f64 := float64(f) * float64(ratio)
+
+						a := strconv.FormatFloat(f64, 'f', 2, 64)
+						ff, _ := strconv.ParseFloat(a, 64)
+
+						value = append(value, ff)
+					}
+					// util.FormatOutput(value)
+					return value
+				}
+
+			}
+		}
+
+	case "bcd":
+		switch strings.ToLower(storeMode) {
+		case "little":
+			fallthrough
+		case "big":
+			fallthrough
+		default:
+			c := make([]string, 2*len(b))
+			s := ""
+			if len(b) > 0 {
+
+				switch strings.ToLower(IFDataType) {
+				case "int":
+					d, err := strconv.Atoi(s)
+					if err != nil {
+
+					}
+					f32 := float32(d) * float32(ratio)
+					value := make([]int, 0)
+					value = append(value, int(f32))
+					return value
+				case "float32":
+					d, err := strconv.Atoi(s)
+					if err != nil {
+
+					}
+
+					f32 := float32(d) * float32(ratio)
+
+					value := make([]float32, 0)
+
+					a := strconv.FormatFloat(float64(f32), 'f', 2, 32)
+					f, _ := strconv.ParseFloat(a, 32)
+
+					value = append(value, float32(f))
+					// util.FormatOutput(value)
+					return value
+
+				case "float64":
+					d, err := strconv.Atoi(s)
+					if err != nil {
+
+					}
+					f64 := float64(d) * float64(ratio)
+
+					value := make([]float64, 0)
+
+					a := strconv.FormatFloat(f64, 'f', 2, 64)
+					f, _ := strconv.ParseFloat(a, 64)
+
+					value = append(value, f)
+					// util.FormatOutput(value)
+					return value
+				case "string":
+					for i := 0; i < len(b); i++ {
+						c[i*2] = strconv.Itoa(int((b[i] & 0b11110000) >> 4))
+						c[i*2+1] = strconv.Itoa(int(b[i] & 0b00001111))
+						s += c[i*2]
+						s += c[i*2+1]
+					}
+					value := make([]string, 0)
+					value = append(value, s)
+					return value
+				}
+			}
+
+		}
+	}
+
+	var value interface{} //???
+	return value
+
+}
---
+++ encode/value2byte.go
@@ -0,0 +1,455 @@
+// value2byte.go
+package util_encode
+
+import (
+	"encoding/binary"
+	"math"
+
+	"strconv"
+	"strings"
+)
+
+func Value2Byte(storeMode string, quantity int, StoreDataType string, IFDataType string, ratio float64, value interface{}) []byte {
+	var b []byte
+	var s []byte
+	switch strings.ToLower(IFDataType) {
+	case "string":
+		v, ok := value.(string)
+		if ok {
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "bcd":
+					var bcd_msb int
+					var bcd_lsb int
+					var valid bool = true
+					b := make([]byte, 0)
+					temp := make([]byte, int(math.Ceil(float64(len(v))/2.0)))
+					for i := 0; i < len(v); i++ {
+						if !(v[i] == '0' || v[i] == '1' || v[i] == '2' || v[i] == '3' || v[i] == '4' || v[i] == '5' || v[i] == '6' || v[i] == '7' || v[i] == '8' || v[i] == '9') {
+							valid = false
+							break
+						}
+					}
+					if valid == true {
+						for i := 0; i < len(v); i = i + 2 {
+							if i+1 < len(v) {
+								bcd_msb, _ = strconv.Atoi(string(v[i]))
+								bcd_lsb, _ = strconv.Atoi(string(v[i+1]))
+							} else {
+								bcd_msb = 0b00000000
+								bcd_lsb, _ = strconv.Atoi(string(v[i]))
+							}
+							temp[i/2] = (byte(bcd_msb) << 4 & 0b11110000) | (byte(bcd_lsb) & 0b00001111)
+						}
+						// b = append(b, temp...)
+					}
+					for j := len(temp) - 1; j >= 0; j-- {
+						b = append(b, temp[j])
+					}
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "bcd":
+					var bcd_msb int
+					var bcd_lsb int
+					var valid bool = true
+					b := make([]byte, 0)
+					temp := make([]byte, int(math.Ceil(float64(len(v))/2.0)))
+					for i := 0; i < len(v); i++ {
+						if !(v[i] == '0' || v[i] == '1' || v[i] == '2' || v[i] == '3' || v[i] == '4' || v[i] == '5' || v[i] == '6' || v[i] == '7' || v[i] == '8' || v[i] == '9') {
+							valid = false
+							break
+						}
+					}
+					if valid == true {
+						for i := 0; i < len(v); i = i + 2 {
+							if i+1 < len(v) {
+								bcd_msb, _ = strconv.Atoi(string(v[i]))
+								bcd_lsb, _ = strconv.Atoi(string(v[i+1]))
+							} else {
+								bcd_msb = 0b00000000
+								bcd_lsb, _ = strconv.Atoi(string(v[i]))
+							}
+							temp[i/2] = (byte(bcd_msb) << 4 & 0b11110000) | (byte(bcd_lsb) & 0b00001111)
+						}
+						b = append(b, temp...)
+					}
+				}
+			}
+		}
+	case "float64":
+		v, ok := value.(float64)
+		if ok {
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(uint64(v/float64(ratio))))
+				case "int16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(int16(int64(v/float64(ratio)))))
+				case "float32":
+					b = make([]byte, 4)
+					bits := math.Float32bits(float32(v / ratio))
+					binary.LittleEndian.PutUint32(b, bits)
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(uint64(v/float64(ratio))))
+				case "int16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(int16(int64(v/float64(ratio)))))
+				case "float32":
+					b = make([]byte, 4)
+					bits := math.Float32bits(float32(v / ratio))
+					binary.BigEndian.PutUint32(b, bits)
+				}
+			}
+		}
+	case "float32":
+		v, ok := value.(float32)
+		if ok {
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(uint32(v/float32(ratio))))
+				case "int16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(int16(int32(v/float32(ratio)))))
+				case "float32":
+					b = make([]byte, 4)
+					bits := math.Float32bits(v / float32(ratio))
+					binary.LittleEndian.PutUint32(b, bits)
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(uint32(v/float32(ratio))))
+				case "int16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(int16(int32(v/float32(ratio)))))
+				case "float32":
+					b = make([]byte, 4)
+					bits := math.Float32bits(v / float32(ratio))
+					binary.BigEndian.PutUint32(b, bits)
+				}
+			}
+
+		}
+	case "int":
+		v, ok := value.(int)
+		if ok {
+
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(uint(float64(v)/ratio)))
+				case "int16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(int16(int(float64(v)/ratio))))
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(uint(float64(v)/ratio)))
+				case "int16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(int16(int(float64(v)/ratio))))
+				}
+			}
+		}
+	case "int64":
+		v, ok := value.(int64)
+		if ok {
+
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(uint64(float64(v)/ratio)))
+				case "int16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(int16(int64(float64(v)/ratio))))
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(uint64(float64(v)/ratio)))
+				case "int16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(int16(int64(float64(v)/ratio))))
+
+				}
+			}
+		}
+	case "int32":
+		v, ok := value.(int32)
+		if ok {
+
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(uint32(float32(v)/float32(ratio))))
+				case "int16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(int16(int32(float32(v)/float32(ratio)))))
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(uint32(float32(v)/float32(ratio))))
+				case "int16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(int16(int32(float32(v)/float32(ratio)))))
+				}
+			}
+		}
+	case "int16":
+		v, ok := value.(int16)
+		if ok {
+
+			switch strings.ToLower(storeMode) {
+			case "little":
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(uint64(float64(v)/ratio)))
+				case "int16":
+					b = make([]byte, 2)
+					binary.LittleEndian.PutUint16(b, uint16(int16(int64(float64(v)/ratio))))
+				}
+			case "big":
+				fallthrough
+			default:
+				switch strings.ToLower(StoreDataType) {
+				case "uint16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(uint64(float64(v)/ratio)))
+				case "int16":
+					b = make([]byte, 2)
+					binary.BigEndian.PutUint16(b, uint16(int16(int64(float64(v)/ratio))))
+				}
+			}
+		}
+	case "[]float64":
+		slice, ok := value.([]float64)
+		if ok {
+			for _, v := range slice {
+				switch strings.ToLower(storeMode) {
+				case "little":
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(uint64(v/float64(ratio))))
+					case "int16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(int16(int64(v/float64(ratio)))))
+					case "float32":
+						s = make([]byte, 4)
+						bits := math.Float32bits(float32(v) / float32(ratio))
+						binary.LittleEndian.PutUint32(s, bits)
+					}
+				case "big":
+					fallthrough
+				default:
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(uint64(v/float64(ratio))))
+					case "int16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(int16(int64(v/float64(ratio)))))
+					case "float32":
+						s = make([]byte, 4)
+						bits := math.Float32bits(float32(v) / float32(ratio))
+						binary.BigEndian.PutUint32(s, bits)
+					}
+				}
+				b = append(b, s...)
+			}
+
+		}
+	case "[]float32":
+		slice, ok := value.([]float32)
+		if ok {
+			for _, v := range slice {
+				switch strings.ToLower(storeMode) {
+				case "little":
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(uint32(v/float32(ratio))))
+					case "int16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(int16(int32(v/float32(ratio)))))
+					case "float32":
+						s = make([]byte, 4)
+						bits := math.Float32bits(v / float32(ratio))
+						binary.LittleEndian.PutUint32(s, bits)
+					}
+				case "big":
+					fallthrough
+				default:
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(uint32(v/float32(ratio))))
+					case "int16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(int16(int32(v/float32(ratio)))))
+					case "float32":
+						s = make([]byte, 4)
+						bits := math.Float32bits(v / float32(ratio))
+						binary.BigEndian.PutUint32(s, bits)
+					}
+				}
+				b = append(b, s...)
+			}
+
+		}
+	case "[]int":
+		slice, ok := value.([]int)
+		if ok {
+			for _, v := range slice {
+				switch strings.ToLower(storeMode) {
+				case "little":
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(uint(float64(v)/ratio)))
+					case "int16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(int16(int(float64(v)/ratio))))
+					}
+				case "big":
+					fallthrough
+				default:
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(uint(float64(v)/ratio)))
+					case "int16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(int16(int(float64(v)/ratio))))
+					}
+				}
+				b = append(b, s...)
+			}
+		}
+	case "[]int64":
+		slice, ok := value.([]int64)
+		if ok {
+			for _, v := range slice {
+				switch strings.ToLower(storeMode) {
+				case "little":
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(uint64(float64(v)/ratio)))
+					case "int16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(int16(int64(float64(v)/ratio))))
+					}
+				case "big":
+					fallthrough
+				default:
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(uint64(float64(v)/ratio)))
+					case "int16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(int16(int64(float64(v)/ratio))))
+					}
+				}
+				b = append(b, s...)
+			}
+		}
+	case "[]int32":
+		slice, ok := value.([]int32)
+		if ok {
+			for _, v := range slice {
+				switch strings.ToLower(storeMode) {
+				case "little":
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(uint32(float32(v)/float32(ratio))))
+					case "int16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(int16(int32(float32(v)/float32(ratio)))))
+					}
+				case "big":
+					fallthrough
+				default:
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(uint32(float32(v)/float32(ratio))))
+					case "int16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(int16(int32(float32(v)/float32(ratio)))))
+					}
+				}
+				b = append(b, s...)
+			}
+		}
+	case "[]int16":
+		slice, ok := value.([]int16)
+		if ok {
+			for _, v := range slice {
+				switch strings.ToLower(storeMode) {
+				case "little":
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(uint64(float64(v)/ratio)))
+					case "int16":
+						s = make([]byte, 2)
+						binary.LittleEndian.PutUint16(s, uint16(int16(int64(float64(v)/ratio))))
+					}
+				case "big":
+					fallthrough
+				default:
+					switch strings.ToLower(StoreDataType) {
+					case "uint16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(uint64(float64(v)/ratio)))
+					case "int16":
+						s = make([]byte, 2)
+						binary.BigEndian.PutUint16(s, uint16(int16(int64(float64(v)/ratio))))
+					}
+				}
+			}
+			b = append(b, s...)
+		}
+	}
+	return b
+}
